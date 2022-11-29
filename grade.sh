c4a862912c22696d52971f3ad12fd04e06c88f84
JemmyChen0221 <114365434+JemmyChen0221@users.noreply.github.com>
2022-11-29 14:29:44 -0800

    .

--- grade.sh
+++ grade.sh
@@ -1,13 +1,16 @@
 # Create your grading script here
 
-set -e
+
 
 rm -rf student-submission
+
 git clone $1 student-submission
+
 
 FILE=student-submission/ListExamples.java
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
+
 else 
     echo "$FILE does notexists."
     exit
@@ -23,6 +26,7 @@ cp lib/junit-4.13.2.jar student-test
 cd student-test
 set +e
 javac -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar *.java
+echo $?
 if [[ $? -eq 0 ]]; then 
     echo "Succefully Compliled"
 else
@@ -31,7 +35,6 @@ else
 fi
 
 java -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
-
 tail -3 output.txt > report.txt
 cat report.txt
 
